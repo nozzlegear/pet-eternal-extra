63ea04b9dd7d11e20b096e5b5703843a5c4a682c
Joshua Harms <joshua@nozzlegear.com>
2019-07-16 10:08:11 -0500

    Replace bluebird with native promises

--- api.ts
+++ api.ts
@@ -1,26 +1,20 @@
-import * as Bluebird from "bluebird";
 import * as Req from "reqwest";
 
-export default class Client
-{
-    constructor(private apiKey: string)
-    {
+export default class Client {
+	constructor(private apiKey: string) {}
 
-    }
+	public uploadBase64(base64: string) {
+		const request = Req({
+			url: "https://pet-eternal-api.azurewebsites.net/api/v1/images",
+			method: "POST",
+			contentType: "application/json",
+			data: JSON.stringify({ base64: base64 }),
+			headers: {
+				"Content-Type": "application/json",
+				"x-pet-eternal-api-key": this.apiKey
+			}
+		});
 
-    public uploadBase64(base64: string)
-    {
-        const request = Req({
-            url: "https://pet-eternal-api.azurewebsites.net/api/v1/images",
-            method: "POST",
-            contentType: "application/json",
-            data: JSON.stringify({base64: base64}),
-            headers: {
-                "Content-Type": "application/json",
-                "x-pet-eternal-api-key": this.apiKey,
-            }
-        });
-
-        return Bluebird.resolve<{filename: string; fullAzureUrl: string; thumbnailAzureUrl: string;}>(request);
-    } 
+		return Promise.resolve<{ filename: string; fullAzureUrl: string; thumbnailAzureUrl: string }>(request);
+	}
 }
