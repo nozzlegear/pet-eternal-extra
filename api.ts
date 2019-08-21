b91118c5afe5e4149fe5bb3e87fb04c441efa45e
Joshua Harms <joshua@nozzlegear.com>
2019-08-21 13:33:40 -0500

    Bugfix: replace reqwest with fetch
    Using reqwest would give errors about the import not being a function. No need to use reqwest in the first place when all targeted browsers now support fetch.

--- api.ts
+++ api.ts
@@ -1,20 +1,29 @@
-import * as Req from "reqwest";
+interface UploadResponse {
+    filename: string;
+    fullAzureUrl: string;
+    thumbnailAzureUrl: string
+}
 
 export default class Client {
-	constructor(private apiKey: string) {}
+    constructor(private apiKey: string) {
+    }
 
-	public uploadBase64(base64: string) {
-		const request = Req({
-			url: "https://pet-eternal-api.azurewebsites.net/api/v1/images",
-			method: "POST",
-			contentType: "application/json",
-			data: JSON.stringify({ base64: base64 }),
-			headers: {
-				"Content-Type": "application/json",
-				"x-pet-eternal-api-key": this.apiKey
-			}
-		});
+    public async uploadBase64(base64: string): Promise<UploadResponse> {
+        const result = await fetch("https://pet-eternal-api.azurewebsites.net/api/v1/images", {
+            method: "POST",
+            body: JSON.stringify({base64: base64}),
+            headers: {
+                "Content-Type": "application/json",
+                "x-pet-eternal-api-key": this.apiKey
+            },
+        });
 
-		return Promise.resolve<{ filename: string; fullAzureUrl: string; thumbnailAzureUrl: string }>(request);
-	}
+        if (result.status !== 200 && result.status !== 201) {
+            const body = await result.text();
+            console.error("Failed to upload image.", {text: body, result: result});
+            throw new Error(`Failed to upload image. Response returned ${result.status} ${result.status}`);
+        }
+
+        return result.json();
+    }
 }
